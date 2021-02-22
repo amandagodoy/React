cbb8622c0433149b08a0f254a8cd2ea4d6bad79e
amandagodoy <amandagodoylima@gmail.com>
2021-02-22 09:39:00 -0300

    Initial commit

---
+++ Projeto_inicial/cliente.js
@@ -0,0 +1,25 @@
+const informacoesClientes = [
+    {
+        cpf: 66809433023,
+        nome: "Jairo"
+    }, {
+        cpf: 66809433023,
+        nome: "Monica"
+    }
+]
+
+const corpoTabela = document.querySelector("[data-conteudo-tabela]");
+
+const exibeCliente = (cpf, nome) => {
+    const linha = document.createElement('tr');
+    const conteudoLinha = 
+        `<td>${cpf}</td>
+        <td>${nome}</td>`
+
+    linha.innerHTML = conteudoLinha
+    return linha
+}
+
+informacoesClientes.forEach(indice => {
+    corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome))
+})
